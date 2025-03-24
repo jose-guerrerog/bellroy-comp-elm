d6ebd6135bf9df17e30031b482066a8dfb27fc14
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-25 09:40:42 +1100

    fix build

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 // vitest.config.ts
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath } from 'url';
+import elm from 'vite-plugin-elm';
+
 import path from 'path';
 
 // Convert URL to filepath for import.meta.url
@@ -8,6 +10,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  plugins: [elm()],
+
+  build: {
+    rollupOptions: {
+      external: [/^elm-stuff\//]
+    }
+  },
+
   test: {
     // Use the jsdom environment for DOM testing
     environment: 'jsdom',
